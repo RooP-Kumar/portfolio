262001976354ed3bc380f6630d665d28f1fedd1a
Roop Kumar <roopkm12@gmail.com>
2024-07-06 17:35:28 +0530

    Update accounts privacy policy, favicon and title

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,13 @@
+import './index.css'
+
 function Home() {
 
   return (
     <>
       <p>Hello There! this is the main function of this programe.</p>
-
       <p>This is the second line of page.</p>
     </>
-  )
+  );
 }
 
 export default Home
