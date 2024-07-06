361f67dca594a92aa07f741d1f6e832763a40669
Roop Kumar <roopkm12@gmail.com>
2024-07-06 11:12:40 +0530

    Initial Commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+
+  return (
+    <>
+      <p>Hello There! this is the main function of this programe.</p>
+
+      <p>This is the second line of page.</p>
+    </>
+  )
+}
+
+export default Home
