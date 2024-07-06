d7814bd98d80cdb04b0b410939a09fd403e9d90d
Roop Kumar <69110669+RooP-Kumar@users.noreply.github.com>
2024-07-06 14:50:51 +0530

    Update PrivacyPolicy.jsx

--- src/components/accounts/PrivacyPolicy.jsx
+++ src/components/accounts/PrivacyPolicy.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function PrivacyPolicy() {
   return (
     <>
-        
+        <p>This is privacy policy page for accounts applicationl.</p>
     </>
   )
 }
