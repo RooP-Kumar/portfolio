f42c062fb90fa67d74b03dd256343ed3bdec3ea8
Roop Kumar <roopkm12@gmail.com>
2024-07-06 12:55:09 +0530

    add accounts privacy policy

---
+++ src/components/accounts/Accounts.jsx
@@ -0,0 +1,11 @@
+
+
+const Accounts = () => {
+    return (
+        <>
+            <p>This is Accounts Android project Details.</p>
+        </>
+    )
+}
+
+export default Accounts
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Accounts from './components/accounts/Accounts.jsx'
+import PrivacyPolicy from './components/accounts/PrivacyPolicy.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home/>
+  },
+  {
+    path: '/accounts',
+    element : <Accounts/>
+  },
+  {
+    path: "/accounts/privacypolicy",
+    element: <PrivacyPolicy/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
